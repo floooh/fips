f525d04e978c35132db6ff77f455cf22b486482f
Andre Weissflog <floooh@gmail.com>
2014-12-22 00:37:57 +0100

    Allow resuse-addr at http server start

--- mod/httpserver.py
+++ mod/httpserver.py
@@ -7,9 +7,12 @@ import log
 
 try :
     log.colored(log.GREEN, 'serving on http://localhost:8000 (Ctrl-C to quit)')
+    SocketServer.TCPServer.allow_reuse_address = True
     httpd = SocketServer.TCPServer(('localhost', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
     httpd.serve_forever()
 except KeyboardInterrupt:
+    httpd.shutdown()
+    httpd.server_close()
     log.colored(log.GREEN, '\nhttp server stopped')
     exit(0)
 
