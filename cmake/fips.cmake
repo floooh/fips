4ec3033572338d6421cee653e24d7cd22dc0a3a6
Andre Weissflog <floooh@gmail.com>
2015-02-24 19:19:34 +0100

    Wrap cmake_policy CMP0042 with a version check

--- cmake/fips.cmake
+++ cmake/fips.cmake
@@ -3,7 +3,9 @@
 #   Main cmake header for fips, this must be included in the top-level
 #   CMakeLists.txt file of a fips project
 #-------------------------------------------------------------------------------
-cmake_policy(SET CMP0042 NEW)
+if (${CMAKE_VERSION} VERSION_GREATER 3.0) 
+    cmake_policy(SET CMP0042 NEW)
+endif()
 
 get_filename_component(FIPS_PROJECT_DIR "." ABSOLUTE)
 get_filename_component(FIPS_DEPLOY_DIR "../fips-deploy" ABSOLUTE)
