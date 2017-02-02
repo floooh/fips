42518ed7456094f7193163e00f3feddfde12b9fa
Andre Weissflog <floooh@gmail.com>
2017-02-02 16:27:06 +0100

    Revert "genutil.py: ignore version stamp for unknown file types"
    (broke fips test)
    
    This reverts commit dde32c6a2aa6fc668106e8e3dec7e5a91bdb2214.

--- generators/genutil.py
+++ generators/genutil.py
@@ -52,23 +52,18 @@ def fileVersionDirty(filePath, version) :
     $$version:X statemenet in it, returns False if the version
     number in the file is equal to the arg version.
     '''
-    if os.path.isfile(filePath):
-        f = open(filePath, 'r')
-        for i in range(0,4) :
-            line = f.readline()
-            startIndex = line.find('#version:')
-            if startIndex != -1 :
-                endIndex = line.find('#', startIndex + 9)
-                if endIndex != -1 :
-                    versionNumber = line[startIndex + 9 : endIndex]
-                    if int(versionNumber) == version :
-                        return False
-            else:
-                # file exists but no version tag, this might be a binary file,
-                # return 'not dirty', and only use time stamp
-                return False
+    f = open(filePath, 'r')
+    for i in range(0,4) :
+        line = f.readline()
+        startIndex = line.find('#version:')
+        if startIndex != -1 :
+            endIndex = line.find('#', startIndex + 9)
+            if endIndex != -1 :
+                versionNumber = line[startIndex + 9 : endIndex]
+                if int(versionNumber) == version :
+                    return False
 
-    # fallthrough: file does not exist, or version tag exists but doesn't match
+    # fallthrough: no version or version doesn't match
     return True
 
 #-------------------------------------------------------------------------------
