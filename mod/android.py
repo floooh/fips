178a0d00c74d7f63279482d82fddc78e18dcf270
Andre Weissflog <floooh@gmail.com>
2018-01-24 14:19:31 +0100

    Start with new Android SDK support

--- mod/android.py
+++ mod/android.py
@@ -8,67 +8,37 @@ import subprocess
 from mod import log, util 
 from mod.tools import java
 
-sdk_urls = {
-    'win' :     'http://dl.google.com/android/android-sdk_r22.6.2-windows.zip',
-    'osx' :     'http://dl.google.com/android/android-sdk_r22.6.2-macosx.zip',
-    'linux' :   'http://dl.google.com/android/android-sdk_r22.6.2-linux.tgz'
+tools_urls = {
+    'win':      'https://dl.google.com/android/repository/sdk-tools-windows-3859397.zip',
+    'osx':      'https://dl.google.com/android/repository/sdk-tools-darwin-3859397.zip',
+    'linux':    'https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip'
 }
 
-sdk_archives = {
-    'win' :     'android-sdk_r22.6.2-windows.zip',
-    'osx' :     'android-sdk_r22.6.2-macosx.zip',
-    'linux' :   'android-sdk_r22.6.2-linux.tgz'
+tools_archives = {
+    'win':      'sdk-tools-windows-3859397.zip',
+    'osx':      'sdk-tools-darwin-3859397.zip',
+    'linux':    'sdk-tools-linux-3859397.zip'
 }
-
-sdk_paths = { 
-    'win' :     'android-sdk-windows',
-    'osx' :     'android-sdk-macosx',
-    'linux' :   'android-sdk-linux'    
-}
-
-ndk_urls = {
-    'win' :     'http://dl.google.com/android/ndk/android-ndk-r9d-windows-x86.zip',
-    'osx' :     'http://dl.google.com/android/ndk/android-ndk-r9d-darwin-x86_64.tar.bz2',
-    'linux' :   'http://dl.google.com/android/ndk/android-ndk-r9d-linux-x86_64.tar.bz2'
-}
-
-ndk_archives = {
-    'win' :     'android-ndk-r9d-windows-x86.zip',
-    'osx' :     'android-ndk-r9d-darwin-x86_64.tar.bz2',
-    'linux' :   'android-ndk-r9d-linux-x86_64.tar.bz2'
-}
-
-#-------------------------------------------------------------------------------
-def get_sdk_url() :
-    return sdk_urls[util.get_host_platform()]
-
-#-------------------------------------------------------------------------------
-def get_ndk_url() :
-    return ndk_urls[util.get_host_platform()]
 
 #-------------------------------------------------------------------------------
 def get_sdk_dir(fips_dir) :
-    return util.get_workspace_dir(fips_dir) + '/fips-sdks/' + util.get_host_platform()
+    return util.get_workspace_dir(fips_dir) + '/fips-sdks/' + util.get_host_platform() + '/android/'
 
 #-------------------------------------------------------------------------------
-def get_androidsdk_dir(fips_dir) :
-    return get_sdk_dir(fips_dir) + '/' + sdk_paths[util.get_host_platform()]
+def get_tools_url() :
+    return tools_urls[util.get_host_platform()]
 
 #-------------------------------------------------------------------------------
-def get_androidndk_dir(fips_dir) :
-    return get_sdk_dir(fips_dir) + '/android-ndk-r9d'
+def get_tools_archive_path(fips_dir):
+    return get_sdk_dir(fips_dir) + '/' + tools_archives[util.get_host_platform()]
 
 #-------------------------------------------------------------------------------
-def get_androidsdk_archive_path(fips_dir) :
-    return get_sdk_dir(fips_dir) + '/' + sdk_archives[util.get_host_platform()]
-
-#-------------------------------------------------------------------------------
-def get_androidndk_archive_path(fips_dir) :
-    return get_sdk_dir(fips_dir) + '/' + ndk_archives[util.get_host_platform()]
-
-#-------------------------------------------------------------------------------
-def get_adb_path(fips_dir) :
-    return get_androidsdk_dir(fips_dir) + '/platform-tools/adb'
+def install_package(fips_dir, pkg):
+    log.colored(log.BLUE, '>>> install Android SDK package: {}'.format(pkg))
+    sdkmgr_dir = get_sdk_dir(fips_dir) + 'tools/bin/'
+    sdkmgr_path = sdkmgr_dir + 'sdkmanager'
+    cmd = '{} --verbose {}'.format(sdkmgr_path, pkg)
+    subprocess.call(cmd, cwd=sdkmgr_dir, shell=True)
 
 #-------------------------------------------------------------------------------
 def ensure_sdk_dirs(fips_dir) :
@@ -77,25 +47,13 @@ def ensure_sdk_dirs(fips_dir) :
 
 #-------------------------------------------------------------------------------
 def uncompress(fips_dir, path) :
-    if '.zip' in path :
+    # the python zip module doesn't preserve the executable flags, so just
+    # call unzip on Linux and OSX
+    if util.get_host_platform() in ['osx', 'linux']:
+        subprocess.call('unzip {}'.format(path), cwd=get_sdk_dir(fips_dir), shell=True)
+    else:
         with zipfile.ZipFile(path, 'r') as archive:
             archive.extractall(get_sdk_dir(fips_dir))
-    elif '.bz2' or '.tgz' in path :
-        # note: for some reason python's tarfile
-        # module cannot completely unpack the 
-        # Android NDK tar.gz2 file (tested on OSX with python 2.7),
-        # so fall back to command line tar
-        subprocess.call('tar -xvf {}'.format(path), cwd=get_sdk_dir(fips_dir), shell=True)
-
-#-------------------------------------------------------------------------------
-def update_android_sdk(fips_dir, proj_dir) :
-    # FIXME: hardcoded version numbers should be configurable
-    if util.get_host_platform() == 'win' :
-        cmd = '{}/tools/android.bat update sdk -f -u --all --filter tools,platform-tools,build-tools-19.1.0,android-19'.format(get_androidsdk_dir(fips_dir))
-    else :
-        cmd = 'sh {}/tools/android update sdk -f -u --all --filter tools,platform-tools,build-tools-19.1.0,android-19'.format(get_androidsdk_dir(fips_dir))
-    print(cmd)
-    subprocess.call(cmd, cwd=fips_dir, shell=True)
 
 #-------------------------------------------------------------------------------
 def setup(fips_dir, proj_dir) :
@@ -109,29 +67,20 @@ def setup(fips_dir, proj_dir) :
 
     ensure_sdk_dirs(fips_dir)
 
-    # download and setup the Android SDK
-    sdk_archive_path = get_androidsdk_archive_path(fips_dir)
-    if not os.path.isfile(sdk_archive_path) :        
-        sdk_url = get_sdk_url()
-        log.info("downloading '{}'...".format(sdk_url))
-        urllib.urlretrieve(sdk_url, sdk_archive_path, util.url_download_hook)
-    else :
-        log.info("'{}' already exists".format(sdk_archive_path))
-    log.info("\nunpacking '{}'...".format(sdk_archive_path))
-    uncompress(fips_dir, sdk_archive_path)
-    log.info("downloading additional SDK files...")
-    update_android_sdk(fips_dir, proj_dir)
+    # download the command line tools archive
+    tools_archive_path = get_tools_archive_path(fips_dir)
+    tools_url = get_tools_url()
+    log.info("downloading '{}'...".format(tools_url))
+    urllib.urlretrieve(tools_url, tools_archive_path, util.url_download_hook)
+    log.info("\nunpacking '{}'...".format(tools_archive_path))
+    uncompress(fips_dir, tools_archive_path)
 
-    # download the Android NDK
-    ndk_archive_path = get_androidndk_archive_path(fips_dir)
-    if not os.path.isfile(ndk_archive_path) :
-        ndk_url = get_ndk_url()
-        log.info("downloading '{}'...".format(ndk_url))
-        urllib.urlretrieve(ndk_url, ndk_archive_path, util.url_download_hook)
-    else :
-        log.info("'{}' already exists".format(ndk_archive_path))
-    log.info("\nunpacking '{}'...".format(ndk_archive_path))
-    uncompress(fips_dir, ndk_archive_path)
+    # install the required SDK components through sdkmanager
+    install_package(fips_dir, '"platforms;android-21"')
+    install_package(fips_dir, '"build-tools;27.0.3"')
+    install_package(fips_dir, 'emulator')
+    install_package(fips_dir, 'platform-tools')
+    install_package(fips_dir, 'ndk-bundle')
 
     log.colored(log.GREEN, "done.")
 
