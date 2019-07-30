83fac7ed7370f9c4dfdf1fecca26c4201d6cf425
Daniel Ludwig <codi@code-disaster.com>
2019-07-30 10:36:28 +0200

    add 'fips update fips' to perform an self-update

--- verbs/update.py
+++ verbs/update.py
@@ -1,21 +1,31 @@
 """update dependencies (only if no local changes)
 
-update all      -- update all dependencies
+update          -- update all dependencies
 update [proj]   -- update a specific dependency
+update fips     -- update fips itself
 """
 
 from mod import log, util, dep
+from mod.tools import git
 
 #-------------------------------------------------------------------------------
 def run(fips_dir, proj_dir, args) :
-    if len(args) > 0 :
-        proj_name = args[0]
-        proj_dir = util.get_project_dir(fips_dir, proj_name)
-    dep.update_imports(fips_dir, proj_dir)
+    if len(args) > 1 and args[0] == 'fips' :
+        if not git.has_local_changes(fips_dir) :
+            log.warn("  '{}' has local changes, skipping...".format(fips_dir))
+        else :
+            log.colored(log.BLUE, "  updating '{}'...".format(fips_dir))
+            git.update(fips_dir)
+    else :
+        if len(args) > 0 :
+            proj_name = args[0]
+            proj_dir = util.get_project_dir(fips_dir, proj_name)
+        dep.update_imports(fips_dir, proj_dir)
 
 #-------------------------------------------------------------------------------
 def help() :
-    log.info(log.YELLOW + 
-            "fips update\n" 
-            "fips update [proj]\n" + log.DEF +
-            "    update external dependencies for current or named project") 
+    log.info(log.YELLOW +
+            "fips update\n"
+            "fips update [proj|fips]\n" + log.DEF +
+            "    update external dependencies for current or named project,\n"
+            "    or update fips itself")
