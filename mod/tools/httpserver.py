6f0dbf5ab9830165da30d93390df7bb4a36e3327
Andre Weissflog <floooh@gmail.com>
2020-01-06 11:24:48 +0100

    fix diag-tools check for http-server, fixes #248

--- mod/tools/httpserver.py
+++ mod/tools/httpserver.py
@@ -1,5 +1,5 @@
 """
-    wrapper for node's http-server module, this is preferred over 
+    wrapper for node's http-server module, this is preferred over
     python's SimpleHTTPServer module because it supports
     HTTP range requests
 """
@@ -13,7 +13,7 @@ not_found = "required for running emscripten targets (npm install http-server -g
 #-------------------------------------------------------------------------------
 def check_exists(fips_dir) :
     try:
-        out = subprocess.check_output(['make', '-h'])
+        out = subprocess.check_output(['http-server', '-h'])
         return True
     except (OSError, subprocess.CalledProcessError):
         return False
