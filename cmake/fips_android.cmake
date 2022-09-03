875f45d511b8284f8d442bb7121405bdb4750142
Andre Weissflog <floooh@gmail.com>
2022-09-03 13:42:14 +0200

    fips_android.cmake: don't call hardwired 'python'

--- cmake/fips_android.cmake
+++ cmake/fips_android.cmake
@@ -8,7 +8,7 @@
 #
 macro(fips_android_postbuildstep target)
     add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND python ${FIPS_ROOT_DIR}/tools/android-create-apk.py
+        COMMAND ${PYTHON} ${FIPS_ROOT_DIR}/tools/android-create-apk.py
         --path ${CMAKE_CURRENT_BINARY_DIR}
         --name ${target}
         --abi ${CMAKE_ANDROID_ARCH_ABI}
